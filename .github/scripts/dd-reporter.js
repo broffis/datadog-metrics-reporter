01c5242cf9407f73bb91efe05159cf4ffd6eca1d
Brandon Roffis <brandon.roffis@ferguson.com>
2023-05-30 17:06:44 -0400

    feat(action): started gh action

---
+++ .github/scripts/dd-reporter.js
@@ -0,0 +1,44 @@
+// import { client, v2 } from "@datadog/datadog-api-client";
+
+module.exports = ({ core }) => {
+  const inputData = core.getInput("data");
+  // const configuration = client.createConfiguration();
+  // const apiInstance = new v2.MetricsApi(configuration);
+  // const params = {
+  //   body: {
+  //     series: [
+  //       {
+  //         metric: "cwv.nightly.largestContentfulPaint",
+  //         type: 0,
+  //         points: [
+  //           {
+  //             timestamp: Math.round(new Date().getTime() / 1000),
+  //             value: 10418.11,
+  //           },
+  //         ],
+  //         resources: [
+  //           {
+  //             name: "wpt",
+  //             type: "host",
+  //           },
+  //         ],
+  //         tags: [
+  //           "env:dev",
+  //           "cwv:proof",
+  //           "wpt:github",
+  //           "page:product-summary",
+  //           "device:mobile",
+  //         ],
+  //       },
+  //     ],
+  //   },
+  // };
+  // apiInstance
+  //   .submitMetrics(params)
+  //   .then((data) => {
+  //     console.log(
+  //       "API called successfully. Returned data: " + JSON.stringify(data)
+  //     );
+  //   })
+  //   .catch((error) => console.error(error));
+};
---
+++ .github/scripts/mock-data.js
@@ -0,0 +1,19 @@
+module.exports = ({ core }) => {
+  core.setOuput(
+    "data",
+    JSON.stringify({
+      cruxData: {
+        firstContentfulPaint: 1145.6666666666667,
+        largestContentfulPaint: 10418.111111111111,
+        timeToInteractive: 10896.777777777777,
+        firstInputDelay: 1451.4444444444443,
+        cumulativeLayoutShift: 0.1019894339777778,
+        timeToFirstByte: 550.2222222222222,
+        totalBlockingTime: 9751.111111111111,
+        speedIndex: 4688.222222222223,
+      },
+      pageName: "product-summary",
+      device: "mobile",
+    })
+  );
+};
