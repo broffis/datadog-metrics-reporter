9c7d362001f43fe72a9c95cb251e710204eaba5c
Brandon Roffis <brandon.roffis@ferguson.com>
2023-06-01 10:17:15 -0400

    test action inputs

--- .github/scripts/dd-reporter.js
+++ .github/scripts/dd-reporter.js
@@ -1,11 +1,11 @@
 // import { client, v2 } from "@datadog/datadog-api-client";
 
 module.exports = ({ core }) => {
-  // const inputData = core.getInput("data");
-  // console.log({ inputData });
+  const inputData = core.getInput("data");
+  console.log({ inputData });
 
-  const envData = process.env.data;
-  console.log({ envData });
+  // const envData = process.env.data;
+  // console.log({ envData });
 
   // const configuration = client.createConfiguration();
   // const apiInstance = new v2.MetricsApi(configuration);
